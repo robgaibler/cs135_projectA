564537541bac22449e1eb70581ece643cbe6f86e
robgaibler <148298456+robgaibler@users.noreply.github.com>
2023-10-18 22:09:57 -0400

    Update load_train_data.py

--- load_train_data.py
+++ load_train_data.py
@@ -18,34 +18,48 @@ if __name__ == '__main__':
     tr_text_list = x_train_data['text'].values.tolist()
     te_text_list = x_test_data['text'].values.tolist()
 
-    # Part 1A: Vectorize and clean data
+# Part 1A: Vectorize and clean data
 
-    vectorizer = CountVectorizer(stop_words='english', token_pattern="[a-z]+", 
+vectorizer = CountVectorizer(stop_words='english', token_pattern="[a-z]+", 
                                  binary=True)
     
-    x_train_NF = vectorizer.fit_transform(tr_text_list).toarray()
-    x_test_NF = vectorizer.fit_transform(te_text_list).toarray()
-    y_train_N = y_train_data['is_positive_sentiment'].values
+x_train_NF = vectorizer.fit_transform(tr_text_list).toarray()
+x_test_NF = vectorizer.transform(te_text_list).toarray()
+y_train_N = y_train_data['is_positive_sentiment'].values
 
-    N, F = x_train_NF.shape
+N, F = x_train_NF.shape
 
-    # Part 1B:
+from sklearn.model_selection import StratifiedKFold
+from sklearn.model_selection import GridSearchCV
+from sklearn.linear_model import LogisticRegression
 
-    num_splits = 5
+# define models and parameters
+model = LogisticRegression()
+solvers = ['lbfgs']
+penalty = ['l2']
+c_values = np.logspace(-9, 6, 30)
+# define grid search and base the success metric on AUROC
+grid = dict(solver=solvers,penalty=penalty,C=c_values)
+cv = StratifiedKFold(n_splits=5, shuffle=True, random_state=1)
+grid_search = GridSearchCV(estimator=model, param_grid=grid, n_jobs=-1, cv=cv, scoring='roc_auc',error_score=0)
+grid_result = grid_search.fit(x_train_NF, y_train_N)
 
-    num_c_vals = 20
-    C_vals = np.logspace(0, 2, num_c_vals)
+# summarize results with the best hyperparamters 
+print("Best: %f using %s" % (grid_result.best_score_, grid_result.best_params_))
+means = grid_result.cv_results_['mean_test_score']
+stds = grid_result.cv_results_['std_test_score']
+params = grid_result.cv_results_['params']
+for mean, stdev, param in zip(means, stds, params):
+    print("%f (%f) with: %r" % (mean, stdev, param))
 
-    error_grid = np.zeros((num_c_vals, num_splits))
+C=grid_result.best_params_['C']
+pen=grid_result.best_params_['penalty']
+sol=grid_result.best_params_['solver']
 
-    kf = KFold(n_splits=num_splits)
+bestmodel= LogisticRegression(C=C, solver=sol, penalty=pen)
+bestmodel.fit(x_train_NF, y_train_N)
+prediction = bestmodel.predict(x_test_NF)
+predictionprob=bestmodel.predict_proba(x_test_NF)[:,1]
 
-    for i in range(C_vals.size):
-        for j, (train_index, test_index) in enumerate(kf.split(x_train_NF)):
-            model = LogisticRegression(C=C_vals[i], solver='lbfgs', max_iter=500)
-
-            model.fit(x_train_NF[train_index], y_train_N[train_index])
-
-            prediction = model.predict(x_train_NF[test_index])
-
-            error_grid[i][j] = mean_absolute_error(y_train_N[test_index], prediction)
+yproba1_test = np.array(predictionprob)
+np.savetxt("yproba1_test.txt", yproba1_test)
